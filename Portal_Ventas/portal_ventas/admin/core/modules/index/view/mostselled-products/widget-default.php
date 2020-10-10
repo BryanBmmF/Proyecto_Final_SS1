6c54a1b452f71f0fc7e338b0da01edd4d5882ced
JesfrinGnZ <jess2298gnz@gmail.com>
2020-10-10 12:07:28 -0600

    Se finaliza el reporte

--- Portal_Ventas/portal_ventas/admin/core/modules/index/view/mostselled-products/widget-default.php
+++ Portal_Ventas/portal_ventas/admin/core/modules/index/view/mostselled-products/widget-default.php
@@ -1,8 +1,11 @@
 <?php
-
-$buys =  BuyData::getAll();
-$coin = ConfigurationData::getByPreffix("general_coin")->val;
-
+$con = Database::getCon();
+$sql = "SELECT SUM(t1.q) AS cantidad, t2.name AS nombre, t2.description AS descripcion, t2.price AS precio_individual,SUM(t1.q)*t2.price AS ingreso FROM buy_product AS t1 
+JOIN product AS t2 ON t1.product_id=t2.id 
+JOIN buy AS t3 ON t1.buy_id=t3.id
+WHERE t3.status_id!=3 AND t2.user_id=".$_SESSION["admin_id"]." group by t2.name,t2.price,t2.description ORDER BY cantidad DESC";
+$result =$con->query($sql);
+$row_cnt = $result->num_rows;
 ?>
         <!-- Main Content -->
 
@@ -21,59 +24,33 @@ $coin = ConfigurationData::getByPreffix("general_coin")->val;
                 <div class="widget-body medium no-padding">
 
                   <div class="table-responsive">
-<?php if(count($buys)>0):?>
-                    <table class="table table-bordered">
-                    <thead>
-                      <th></th>
-                      <th>Operacion</th>
-                      <th>Cliente</th>
-                      <th>SubTotal</th>
-                      <th>Descuento</th>
-                      <th>Total</th>
-                      <th>Metodo de pago</th>
-                      <th>Estado</th>
-                      <th>Fecha</th>
-                      <th></th>
-                    </thead>
-<?php foreach($buys as $b):
-$discount=0;
-?>
-                        <tr>
-                        <td><a href="index.php?view=openbuy&buy_id=<?php echo $b->id; ?>" class="btn btn-xs btn-default">Detalles</a></td>
-                        <td>#<?php echo $b->id; ?></td>
-                        <td><?php echo $b->getClient()->getFullname(); ?></td>
-    <td><?php echo $coin; ?> <?php echo number_format($b->getTotal(),2,".",","); ?></td>
-    <td><?php echo $coin; ?>
-      <?php if($b->coupon_id!=null){
-        $coupon = CouponData::getById($b->coupon_id);
-        $discount = $coupon->val;
-        echo number_format($discount,2,".",",");
-        }else{
-        echo number_format($discount,2,".",",");
+<?php if($row_cnt>0):?>
 
-        }
+  <table class="table table-bordered">
+    <thead>
+        <th>Elementos comprados</th>
+        <th>Nombre</th>
+        <th>Descripcion</th>
+        <th>Precio Individual en Q</th>
+        <th>Ingreso total en Q</th>
+    </thead>
+      <?php
+      		while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            echo "<tr>";
+            echo "<td>".$row['cantidad']."</td>";
+            echo "<td>".$row['nombre']."</td>";
+            echo "<td>".$row['descripcion']."</td>";
+            echo "<td>".$row['precio_individual']."</td>";
+            echo "<td>".$row['ingreso']."</td>";
+            echo "</tr>";  
+
+          }
       ?>
-    </td>
-    <td><?php echo $coin; ?> <?php echo number_format($b->getTotal()-$discount,2,".",","); ?></td>
-                        <td><?php echo $b->getPaymethod()->name; ?></td>
-                        <td><?php echo $b->getStatus()->name; ?></td>
-                        <td><?php echo $b->created_at; ?></td>
-                        <th>
-                          <?php if($b->status_id!=5):?>
-                            <a href="./?action=changestatus&id=<?php echo $b->id;?>&status=2" class="btn btn-info btn-xs"><i class="fa fa-usd"></i></a>
-                            <a href="./?action=changestatus&id=<?php echo $b->id;?>&status=4" class="btn btn-success btn-xs"><i class="fa fa-truck"></i></a>
-                            <a href="./?action=changestatus&id=<?php echo $b->id;?>&status=5" class="btn btn-primary btn-xs"><i class="fa fa-check"></i></a>
-                            <a href="./?action=changestatus&id=<?php echo $b->id;?>&status=3" class="btn btn-danger btn-xs"><i class="fa fa-remove"></i></a>
-                          <?php else:?>
-                            <i class="fa fa-check"></i>
-                          <?php endif;?>
-                        </th>
-                        </tr>
-<?php endforeach; ?>
-                    </table>
+    
+    </table>
 <?php else:?>
   <div class="panel-body">
-  <h1>No hay operaciones</h1>
+  <h1>Sin productos vendidos</h1>
   </div>
 <?php endif; ?>
                   </div>
